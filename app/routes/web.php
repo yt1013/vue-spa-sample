1bc28b5d28bdd17563d3cc6ccc982b366c9364dd
yt1013 <49902079+yt1013@users.noreply.github.com>
2021-02-28 18:45:47 +0900

    Vueを用いたSPAのベース実装 (#1)
    * マイグレーションの修正
    
    * データベース接続情報の追加
    
    * SPAベース実装
    
    * コンパイル済みjsとcssの削除
    
    Co-authored-by: yt1013 <yutaka.tsugu@asia-quest.jp>

--- app/routes/web.php
+++ app/routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('auth')->group(function () {
+    Route::get('/{any?}', function(){
+        return view('home');
+    })->where('any', '.*');
+});
